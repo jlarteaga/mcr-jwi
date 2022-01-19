774e239d29c163b33edfc9d4169e92fa4d50e032
Jorge Luis Arteaga Almaraz <jorgitoaral@gmail.com>
2022-01-18 22:39:34 -0400

    Initial commit

---
+++ src/main/java/dev/jlarteaga/mcrbuilder/McrBuilderApplication.java
@@ -0,0 +1,13 @@
+package dev.jlarteaga.mcrbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McrBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McrBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jlarteaga/mcrbuilder/McrBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jlarteaga.mcrbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McrBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
